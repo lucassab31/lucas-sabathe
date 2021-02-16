fbc9e11834e5aa1375d72000052cf167269b9601
Lucas Sabathé <luqui31@gmail.com>
2021-02-16 13:25:31 +0100

    studies

--- app/js/app.js
+++ app/js/app.js
@@ -6,3 +6,12 @@ function showNav() {
 particlesJS.load('particles-js', 'app/js/particles.json', function() {
     console.log('callback - particles.js config loaded');
 });
+
+function down() {
+    element = document.getElementById("about");
+    let y = element.getBoundingClientRect().top;
+    window.scroll({
+        top: y,
+        behavior: 'smooth'
+    })
+}
