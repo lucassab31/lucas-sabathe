13066754cd1aa072e1a22f24ff243152f586da2e
Lucas Sabathé <luqui31@gmail.com>
2021-02-17 21:37:43 +0100

    contact section

--- app/js/app.js
+++ app/js/app.js
@@ -15,3 +15,55 @@ function down() {
         behavior: 'smooth'
     })
 }
+
+var error = false;
+
+function validation(form) {
+    error = false;
+    var name = form.name;
+    var email = form.email;
+    var subject = form.subject;
+    var message = form.message;
+
+
+    if (name.value === '') {
+        setErrorFor(name, "The name can't be null");
+    } else {
+        setSuccess(name);
+    }
+
+    if (email.value === '') {
+        setErrorFor(email, "The email can't be null");
+    } else {
+        setSuccess(email);
+    }
+
+    if (subject.value === '') {
+        setErrorFor(subject, "The subject can't be null");
+    } else {
+        setSuccess(subject);
+    }
+
+    if (message.value === '') {
+        setErrorFor(message, "The message can't be null");
+    } else {
+        setSuccess(message);
+    }
+
+    if (error == false) {
+        // form.submitF.submit();
+    }
+}
+
+function setErrorFor(input, message) {
+    error = true;
+    var formControl = input.parentElement;
+    var small = formControl.querySelector('small');
+    small.innerHTML = message;
+    formControl.className= "form-control error";
+}
+
+function setSuccess(input) {
+    var formControl = input.parentElement;
+    formControl.className= "form-control success";
+}
