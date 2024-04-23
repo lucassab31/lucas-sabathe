f5f557745e622dd302d47255702af48f5693e24c
lucas <luqui31@gmail.com>
2024-04-23 15:37:35 +0200

    projects

---
+++ V2/assets/js/app.js
@@ -0,0 +1,19 @@
+const sliders = document.querySelectorAll(".slide-in");
+const appearOptions = {
+    rootMargin: "0px 0px -200px 0px"
+}
+
+const appearOnScroll = new IntersectionObserver(function (entries, appearOnScroll) {
+    entries.forEach(entry => {
+        if (!entry.isIntersecting) {
+            return;
+        } else {
+            entry.target.classList.add('appear');
+            appearOnScroll.unobserve(entry.target);
+        }
+    })
+}, appearOptions);
+
+sliders.forEach(slider => {
+    appearOnScroll.observe(slider);
+})
